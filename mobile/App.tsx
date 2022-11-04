006920d6b57d18a650b72e31bac8a8ba805b49ab
jessica-guedes <jessicaguedes.ads@gmail.com>
2022-11-04 00:24:07 -0400

    update mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,28 @@
+import React from "react";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
-import { SignIn } from './src/screens/SignIn';
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import { THEME} from './src/styles/theme'
-import React from "react";
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      {fontsLoaded ?  <SignIn /> : <Loading />}
+        {fontsLoaded ?  <Pools /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
---
+++ mobile/src/screens/Pools.tsx
@@ -0,0 +1,9 @@
+import { VStack } from "native-base";
+
+export function Pools(){
+  return(
+    <VStack flex={1} bgColor="gray.950">
+
+    </VStack>
+  )
+}
