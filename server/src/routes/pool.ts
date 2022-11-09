5713ba97864c2ce876764cb863fb2ac0cfb856dd
jessica-guedes <jessicaguedes.ads@gmail.com>
2022-11-09 15:20:33 -0400

    Update Backend
    Finalizando o backend para integrar com  o mobile.

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -52,7 +52,7 @@ export async function poolRoutes(fastify: FastifyInstance){
     return reply.status(201).send({ code })
   })
 
-  fastify.post('/pools/:id/join', {
+  fastify.post('/pools/join', {
     onRequest: [authenticate]
   }, async (request, reply) => {
     const joinPoolBody = z.object({
@@ -146,6 +146,49 @@ export async function poolRoutes(fastify: FastifyInstance){
     })
 
     return { pools }
+  })
+
+  fastify.get('/pools/:id', {
+    onRequest: [authenticate],
+  }, async (request) => {
+    const getPoolParams = z.object({
+      id: z.string(),
+    })
+
+    const { id } = getPoolParams.parse(request.params)
+
+    const pool = await prisma.pool.findUnique({
+      where:{
+        id,
+      },
+      include:{
+        _count:{
+          select:{
+            participants: true,
+          }
+        },
+        participants:{
+          select:{
+            id: true,
+
+            user:{
+              select: {
+                avatarUrl: true,
+              }
+            }
+          },
+          take: 4,
+        },
+        owner: {
+          select:{
+            id: true,
+            name: true,
+          }
+        }
+      }
+    })
+
+    return { pool }
   })
 
 }
